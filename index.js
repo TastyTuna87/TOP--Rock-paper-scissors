7ce5b779946f384823b862bd58e0aa589d95ea18
TastyTuna87 <szabtor87@gmail.com>
2022-02-20 23:57:39 +0100

    palyerResult ok

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 //Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound 
 //function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
 
-const buttons = document.querySelector("#buttons");
-
 const tools = ["rock", "paper", "scissor"];
 
+const button = document.querySelector("button");
 const rock = document.querySelector("#btn-rock");
 const paper = document.querySelector("#btn-paper");
 const scissor = document.querySelector("#btn-scissor");
 
+const playerScore = document.querySelector("userScore");
+const computerScore = document.querySelector("aiScore");
+const drawScore = document.querySelector("draw");
 
-let playerResult = null;
-console.log(playerResult);
-let userScore = 1;
-let aiScore = 1;
-let draw = 1;
+let userScore = 0;
+let aiScore = 0;
+let draw = 0;
 
-
-rock.addEventListener("click", function onclick(){
-    playerResult = tools[0], console.log("Player: ", playerResult);
-});
-paper.addEventListener("click", function onclick(){
-    return playerResult = tools[1], console.log("Player: ", playerResult);
-});
-scissor.addEventListener("click", function onclick(){
-    return playerResult = tools[2], console.log("Player: ", playerResult);
-});
+function playerSelect(){
+    button.addEventListener("click", () =>{
+        if(button.id == "btn-rock"){
+            return tools[0];
+        }else if(button.id == "btn-paper"){
+            return tools[1];
+        }else if(button.id == "btn-scissor"){
+            return tools[2];
+        }
+    });
+    return  event.target.value;
+    };
 
 //NPC random select
 function computerPlay(){
@@ -36,26 +38,26 @@ function computerPlay(){
 
 // start the game for 1 round
 function playRound(){
-    let aiResult = computerPlay();
+    const aiResult = computerPlay();
+    const playerResult = playerSelect();
     const match = `${playerResult} vs ${aiResult}`;
-
+    
     if(playerResult === aiResult){
-        return console.log(`${match} is a draw!`)
+       return draw++, console.log(`${match} is a draw!`, draw)
     }else{
         if(playerResult === tools[0] && aiResult != tools[1]){
-            return console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`)
+            return userScore++, console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`, userScore++)
         }else if(playerResult === tools[1] && aiResult != tools[2]) {
-            return console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`)
+            return userScore++, console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`, userScore++)
         }else if(playerResult === tools[2] && aiResult != tools[0]){
-            return console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`)
+            return userScore++, console.log(`${match}: You win, ${playerResult} beat the ${aiResult}`,userScore++)
         }else{
-            return console.log(`${match}: Ai win, ${aiResult} beat the ${playerResult}`)
+            return aiScore++, console.log(`${match}: Ai win, ${aiResult} beat the ${playerResult}`, aiScore++)
         }
     }
-
-    
 };
 
+//userScore++
 
 
  
